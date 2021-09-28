12a88c93629951adc45e59256aa435defbd2a8bc
Andrew Patterson <andrew.patterson747@gmail.com>
2021-09-28 01:15:53 -0500

    Add: match_value method

--- lib/dictionary.rb
+++ lib/dictionary.rb
@@ -1,12 +1,32 @@
 class Dictionary
+  attr_reader :dict
 
-  def initialize
-    @nightwriter = NightWriter.new
+  def initialize(dict)
+    @dictionary = dict
   end
 
-  def match_value
-    @nightwriter.split
+  def match_value(let)
+    match = []
+    @dictionary.each do |line|
+      line.select do |letter|
+        if let == letter
+          match = @dictionary[letter]
+        end
+      end
+    end
+    match
   end
+  #
+  # def translate
+  #   message = "This is a test"
+  #   output = ""
+  #   splits = message.split
+  #   splits.each do |word|
+  #     split_word = word.split("")
+  #     split_word.each do |letter|
+  #       output += match_value(letter)
+  #     end
+  #   end
+  #   output
+  # end
 end
-
-Dictionary.new.match_value
